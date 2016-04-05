6571913c971e08a3f4f8a5ad53e2f661cafd69a1
ktran031 <ktran031@gmail.com>
2016-04-05 07:06:12 -0700

    223

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(pageReady);
 function pageReady() {
 	
 
-
+// Nav Bar 
 $(function() {
     var pull        = $('#pull');
         menu        = $('nav ul');
@@ -22,5 +22,29 @@ $(window).resize(function(){
     }
 }); 
 
+// Banner Area //
+    
+    //Click to slide down Why Work With Me?//
+    $(".button").click(blurbToggle) ;
 
+    function blurbToggle () {
+        event.preventDefault();
+        $("#show-this-on-click").slideToggle();
+    }
+ 
+//Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+ 
 }
