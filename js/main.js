48382b5527bbadddc0ccf3f60d721abc18288712
ktran031 <ktran031@gmail.com>
2016-04-06 23:35:11 -0700

    Revert "21d"
    This reverts commit e02e1991927c461bdcc7a38d47f4836a1d989160.

--- js/main.js
+++ js/main.js
@@ -46,46 +46,46 @@ $(window).resize(function(){
 
 // Fixed Nav Bar
 // define variables
-//     var navOffset, scrollPos = 0;
+    // var navOffset, scrollPos = 0;
     
-//     // add utility wrapper elements for positioning
-//     jQuery("nav").wrap('<div class="nav-placeholder"></div>');
-//     jQuery("nav").wrapInner('<div class="nav-inner"></div>');
-//     jQuery(".nav-inner").wrapInner('<div class="nav-inner-most"></div>');
+    // // add utility wrapper elements for positioning
+    // jQuery("nav").wrap('<div class="nav-placeholder"></div>');
+    // jQuery("nav").wrapInner('<div class="nav-inner"></div>');
+    // jQuery(".nav-inner").wrapInner('<div class="nav-inner-most"></div>');
     
-//     // function to run on page load and window resize
-//     function stickyUtility() {
+    // // function to run on page load and window resize
+    // function stickyUtility() {
         
-//         // only update navOffset if it is not currently using fixed position
-//         if (!jQuery("nav").hasClass("fixed")) {
-//             navOffset = jQuery("nav").offset().top;
-//         }
+    //     // only update navOffset if it is not currently using fixed position
+    //     if (!jQuery("nav").hasClass("fixed")) {
+    //         navOffset = jQuery("nav").offset().top;
+    //     }
         
-//         // apply matching height to nav wrapper div to avoid awkward content jumps
-//         jQuery(".nav-placeholder").height(jQuery("nav").outerHeight());
+    //     // apply matching height to nav wrapper div to avoid awkward content jumps
+    //     jQuery(".nav-placeholder").height(jQuery("nav").outerHeight());
         
-//     } // end stickyUtility function
+    // } // end stickyUtility function
     
-//     // run on page load
-//     stickyUtility();
+    // // run on page load
+    // stickyUtility();
     
-//     // run on window resize
-//     jQuery(window).resize(function() {
-//         stickyUtility();
-//     });
+    // // run on window resize
+    // jQuery(window).resize(function() {
+    //     stickyUtility();
+    // });
     
-//     // run on scroll event
-//     jQuery(window).scroll(function() {
+    // // run on scroll event
+    // jQuery(window).scroll(function() {
         
-//         scrollPos = jQuery(window).scrollTop();
+    //     scrollPos = jQuery(window).scrollTop();
         
-//         if (scrollPos >= navOffset) {
-//             jQuery("nav").addClass("fixed");
-//         } else {
-//             jQuery("nav").removeClass("fixed");
-//         }
+    //     if (scrollPos >= navOffset) {
+    //         jQuery("nav").addClass("fixed");
+    //     } else {
+    //         jQuery("nav").removeClass("fixed");
+    //     }
         
-//     });
+    // });
 
 
 // Banner Area //
